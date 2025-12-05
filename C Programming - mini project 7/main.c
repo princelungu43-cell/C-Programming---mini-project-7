21bfda5e3fd9b0fe03c56d4fb3bd14260ae835f5
princelungucell-43 <test@3schools.com>
2025-12-05 13:11:56 +0200

    PROJECT 7

--- C Programming - mini project 7/main.c
+++ C Programming - mini project 7/main.c
@@ -2,13 +2,26 @@
 //  main.c
 //  C Programming - mini project 7
 //
-//  Created by user on 12/10/2025.
+//  Created by PRIMCE LUNGU on 12/10/2025.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main() {
+    int firstN;
+    int secondN;
+    int thirdN;
+    int largest;
+    
+    printf("ENTER FIRST NUMBER: ");
+    scanf("%d\n", &firstN);
+    
+    printf("ENTER SECOND NUMBER: ");
+    scanf("%d", &secondN);
+    
+    printf("ENTER THIRD NUMBER: ");
+           scanf("%d", &thirdN);
+           
+           
+    
 }
